2e164536dbe762d9bbc05aacc376c212e6a707a6
danielnakamashi <danielnakamashi@users.noreply.github.com>
2021-05-29 22:15:26 +0000

    deploy: b60381bf289d7b0f049985d9ced8cf714da26dfc

---
+++ _next/static/EJeq9Vf0DdWmd4SHV2gOh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-bd3e100a11fb0a2c8a4b.js","static/css/2ac64242f574cabb09e8.css","static/chunks/pages/index-b6ff504c1aced296a3e8.js"],"/_error":["static/chunks/pages/_error-c75db885116e2a87dc1c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
